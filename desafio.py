83c5c536415856e7ddb1fd013aa2017226bc87c5
andre <strongerandrebomfim@gmail.com>
2024-05-18 20:15:57 -0300

    Sistema-Bancário

---
+++ desafio.py
@@ -0,0 +1,48 @@
+banco = """
+d = Depositar
+s = sacar
+e = extrato
+q = sair 
+
+Qual opreção você quer realizar?  """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+limite_de_saques = 3
+
+while True:
+    opcao = input(banco)
+
+    if opcao == "d":
+        valor = float(input("Qual o valor do depósito: "))
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito de R$ {valor:.2f}\n"
+        else:
+            print("O valor é inválido! Digite novamente")
+    elif opcao == "s":
+        valor = float(input("Qual o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= limite_de_saques
+
+        if excedeu_saldo or excedeu_limite or excedeu_saques:
+            print("Operação não permitida.")
+        elif valor > 0:
+            saldo -= valor
+            numero_saques += 1
+            extrato += f"Saque de R${valor:.2f}\n"
+        else:
+            print("O valor é inválido! Digite novamente")
+    elif opcao == "e":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+    elif opcao == "q":
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
