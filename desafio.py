9b8ec46ecbd1c53a5ffb5b135d77086363a1400e
andre <strongerandrebomfim@gmail.com>
2024-05-18 21:56:30 -0300

    Sitema-Bancario

--- desafio.py
+++ desafio.py
@@ -1,8 +1,8 @@
 banco = """
-d = Depositar
-s = sacar
-e = extrato
-q = sair 
+d = Depositar ,
+s = sacar ,
+e = extrato ,
+q = sair , 
 
 Qual opreção você quer realizar?  """
 
@@ -29,8 +29,12 @@ while True:
         excedeu_limite = valor > limite
         excedeu_saques = numero_saques >= limite_de_saques
 
-        if excedeu_saldo or excedeu_limite or excedeu_saques:
-            print("Operação não permitida.")
+        if excedeu_saldo:
+            print("Saldo insuficiente.")
+        elif excedeu_limite:
+            print("Limite Excedido.")
+        elif excedeu_saques:
+            print("Operação não permitida. Número de saques foi excedido")
         elif valor > 0:
             saldo -= valor
             numero_saques += 1
@@ -38,10 +42,10 @@ while True:
         else:
             print("O valor é inválido! Digite novamente")
     elif opcao == "e":
-        print("\n================ EXTRATO ================")
+        print("\n ~~~~~~~~~~~~~~~~~~~~~~Extrato~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
-        print("==========================================")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     elif opcao == "q":
         break
     else:
